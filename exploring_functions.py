ad8b8c51c5aa84a733209898f4218297b2b39711
Zaire Ali <zaireali649@gmail.com>
2025-02-20 20:52:59 -0500

    Create exploring_functions.py

---
+++ exploring_functions.py
@@ -0,0 +1,18 @@
+import random
+
+
+def add_random(a, b):
+    number1 = random.randint(a, b)
+    number2 = random.randint(a, b)
+
+    print(number1 + number2)
+
+def add(a, b):
+    return a + b
+
+
+x = random.randint(0, 3)
+y = random.randint(3, 10)
+
+print(x, y)
+add_random(x, y)
